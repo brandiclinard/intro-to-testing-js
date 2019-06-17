b4889a1a092b97d0480923fcff6ce71d1d552ec5
Brandi Clinard <brandi@Brandis-MacBook-Pro.local>
2019-06-17 16:01:30 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -2,3 +2,15 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function add(num1, num2){
+    if(typeof num1 === 'string' || typeof num2 === 'string'){
+        return false;
+    }else{
+        return num1 + num2;
+    }
+}
+
+function sayHello(){
+
+}
